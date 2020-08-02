6e6b72ff4762a67189e5a7d54465282a5210ffe7
zhangjt <zhangjt@btismart.com>
2020-08-02 20:07:58 +0800

    JSX优化

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
-import {ToyReact} from './ToyReact.js';
+import {ToyReact, Component} from './ToyReact.js';
 
-let a = <div name="mine" >
-            <span>1</span>
-            <span>2</span>
-            <span>3</span>
+class MyComponent extends Component{
+    render(){
+        return <div>
+            <span>hello</span>
+            <span>world</span>
+            <div>
+                {true}
+                {this.children}
+            </div>
         </div>
-console.log(document.body);
-document.body.appendChild(a);
+    }
+    
+}
+
+let a = <MyComponent name="a" id="ida">
+    <div>123</div>
+</MyComponent>
+
+ToyReact.render(
+    a,
+    document.body
+)
+
+// let a = <div name="mine" >
+//             <span>1</span>
+//             <span>2</span>
+//             <span>3</span>
+//         </div>
+// document.body.appendChild(a);
